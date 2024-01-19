250b7319cc8d61e1fcadbefa210895d51614f667
David-mwas <dmwas704@gmail.com>
2024-01-19 16:16:51 +0300

    Fix :fix routing to proposals, dashboard, settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+import Proposals from "./pages/Proposals";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route index element={<Homepage />} />
+        <Route index element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        {/* <Route path="/dasboard" element={ } /> */}
-        {/* <Route path="/profile" element={<Profile />} /> */}
+        {/* <Route path="/dasboard" element={<Profile/> } /> */}
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/proposals" element={<Proposals />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  IoLockClosed,
+  IoPencil,
+  IoLockClosedOutline,
+  IoPerson,
+} from "react-icons/io5";
+import {
+  MdLock,
+  MdMail,
+  MdNumbers,
+  MdOutlineEmail,
+  MdPhone,
+} from "react-icons/md";
+import { CiLock, CiPhone } from "react-icons/ci";
+import { Link } from "react-router-dom";
+import Logo from "../assets/MMU.png";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { useSidebar } from "../../hooks/useHandleSideBar";
+function Dashboard() {
+  const { open, handleSideBar, handleSidebarItemClick } = useSidebar();
+  return (
+    <div>
+      <Navbar handleSideBar={handleSideBar} open={open} />
+      <Sidebar
+        open={open}
+        handleSideBar={handleSideBar}
+        // onItemClick={handleSidebarItemClick}
+      />
+      <div className="p-8 h-full justify-start items-start w-full flex text-black md:pl-[250px]  flex-col">
+        <h5 className="text-blue-500 text-xl font-bold uppercase  md:left-[250px] mt-20">
+         Dashboard
+        </h5>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
